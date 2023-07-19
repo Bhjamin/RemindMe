33fcc3acf5647d9c4a53eadc62cb5bd4bbe94d2f
Ben Hancock <benjamin@hancock1.com>
2023-07-19 12:25:31 -0600

    gggg

--- common/twilio.js
+++ common/twilio.js
@@ -1,6 +1,6 @@
 const accountSid = "ACbf760a670c4bf70307b8050664c1cfa8"
 const authToken = "494ce34901e6a26a371e984cb80c9643"
-const client = require("twilio") (accountSid, authToken)
+const client = require("twilio")(accountSid, authToken)
 
 module.exports = {
     client
