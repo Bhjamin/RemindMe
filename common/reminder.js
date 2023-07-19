360c5789a96b69106495086a1ac47ee9dd474685
Ben Hancock <benjamin@hancock1.com>
2023-07-19 09:39:22 -0600

    yes now work

--- common/reminder.js
+++ common/reminder.js
@@ -1,4 +1,20 @@
-const { db } = require('../../src/config/firebase')
+import { initializeApp } from "firebase/app"
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoycbDuyHTxdHaM9QpoMzR99T_Vmjp-4",
+  authDomain: "dailyreminders-62630.firebaseapp.com",
+  projectId: "dailyreminders-62630",
+  storageBucket: "dailyreminders-62630.appspot.com",
+  messagingSenderId: "764364730830",
+  appId: "1:764364730830:web:45df26e2b793c33d3dfb9f",
+  measurementId: "G-4ECKGJWRG0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
 const { getDocs, collection } = require("firebase/firestore")
 
 const accountSid = "ACbf760a670c4bf70307b8050664c1cfa8"
