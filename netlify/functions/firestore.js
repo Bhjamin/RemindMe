c53abdba8f165753c4ee4a41b895348a8c702840
Ben Hancock <benjamin@hancock1.com>
2023-07-19 12:18:39 -0600

    working commit hopefully please

--- netlify/functions/firestore.js
+++ netlify/functions/firestore.js
@@ -25,52 +25,56 @@ const handler = async (event, context) => {
 
         if (today.getHours() >= 8) {
 
+            console.log('passed if')
 
-            if (task5.stringValue && !task5complete.booleanValue) {
-                client.messages
-                    .create({
-                        body: `${task5.stringValue} has not been done yet`,
-                        from: "+18668219659",
-                        to: "+14805491152",
-                    })
-                    .then((message) => console.log("hit", message.sid))
-                    .done()
-            }
+            // if (task5.stringValue && !task5complete.booleanValue) {
+            //     client.messages
+            //         .create({
+            //             body: `${task5.stringValue} has not been done yet`,
+            //             from: "+18668219659",
+            //             to: "+14805491152",
+            //         })
+            //         .then((message) => console.log("hit", message.sid))
+            //         .done()
+            // }
 
-            if (task4.stringValue && !task4complete.booleanValue) {
-                client.messages
-                    .create({
-                        body: `${task4.stringValue} has not been done yet`,
-                        from: "+18668219659",
-                        to: "+14805491152",
-                    })
-                    .then((message) => console.log("hit", message.sid))
-                    .done()
-            }
+            // if (task4.stringValue && !task4complete.booleanValue) {
+            //     client.messages
+            //         .create({
+            //             body: `${task4.stringValue} has not been done yet`,
+            //             from: "+18668219659",
+            //             to: "+14805491152",
+            //         })
+            //         .then((message) => console.log("hit", message.sid))
+            //         .done()
+            // }
 
-            if (task3.stringValue && !task3complete.booleanValue) {
-                client.messages
-                    .create({
-                        body: `${task3.stringValue} has not been done yet`,
-                        from: "+18668219659",
-                        to: "+14805491152",
-                    })
-                    .then((message) => console.log("hit", message.sid))
-                    .done()
-            }
+            // if (task3.stringValue && !task3complete.booleanValue) {
+            //     client.messages
+            //         .create({
+            //             body: `${task3.stringValue} has not been done yet`,
+            //             from: "+18668219659",
+            //             to: "+14805491152",
+            //         })
+            //         .then((message) => console.log("hit", message.sid))
+            //         .done()
+            // }
 
-            if (task2.stringValue && !task2complete.booleanValue) {
-                client.messages
-                    .create({
-                        body: `${task2.stringValue} has not been done yet`,
-                        from: "+18668219659",
-                        to: "+14805491152",
-                    })
-                    .then((message) => console.log("hit", message.sid))
-                    .done()
-            }
+            // if (task2.stringValue && !task2complete.booleanValue) {
+            //     client.messages
+            //         .create({
+            //             body: `${task2.stringValue} has not been done yet`,
+            //             from: "+18668219659",
+            //             to: "+14805491152",
+            //         })
+            //         .then((message) => console.log("hit", message.sid))
+            //         .done()
+            // }
 
             if (!task1complete.booleanValue) {
+
+                console.log("task 1 stupid idiot")
+
                 client.messages
                     .create({
                         body: `You have not worked out today`,
