49ad8a6d9463e199fd2b8cef8009c5fb3b6626cd
Ben Hancock <benjamin@hancock1.com>
2023-07-20 11:34:06 -0600

    ess o comm

--- netlify/functions/firestore.js
+++ netlify/functions/firestore.js
@@ -1,16 +1,13 @@
 const { schedule } = require('@netlify/functions')
-import axios from 'axios'
 
 const handler = async (event, context) => {
 
-    fetch('https://remindme-beng.netlify.app/api/sendReminder')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
+    try {
+       const res = await fetch('https://remindme-beng.netlify.app/api/sendReminder')
+       console.log("RESPONSE: ", res)
+    } catch (err) {
+        console.log(err)
+    }
 
 }
 
