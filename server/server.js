b1ef569f93f40902ee43764e798618a4e6646193
Ben Hancock <benjamin@hancock1.com>
2023-07-20 09:00:03 -0600

    cant stop farging

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const cors = require("cors")
+// const express = require("express")
+// const cors = require("cors")
 
-const app = express()
+// const app = express()
 
-app.use(express.json())
-app.use(cors())
+// app.use(express.json())
+// app.use(cors())
 
-const { handler } = require("./controller")
+// const { handler } = require("./controller")
 
-app.get('/api/sendReminders', handler)
+// app.get('/api/sendReminders', handler)
 
-app.listen(5000, () => {
-    console.log('server running')
-})
+// app.listen(5000, () => {
+//     console.log('server running')
+// })
