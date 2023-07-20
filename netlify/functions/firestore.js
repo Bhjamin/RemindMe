adaedae931ee13f8590ca9215b88e80d55d33ff3
Ben Hancock <benjamin@hancock1.com>
2023-07-20 09:58:15 -0600

    pblleehh

--- netlify/functions/firestore.js
+++ netlify/functions/firestore.js
@@ -1,10 +1,9 @@
 const { schedule } = require('@netlify/functions')
 const axios = require('axios')
 
-
 const handler = async (event, context) => {
 
-axios.get('https://remindme-beng.netlify.app/.netlify/functions/api')
+axios.get('https://remindme-beng.netlify.app/.netlify/functions/api/sendReminder')
 .then((res) => {
     console.log(res.data)
 })
