7357089f511ea3ccbd769bde5c88f57bd46284f8
Ben Hancock <benjamin@hancock1.com>
2023-07-20 10:03:34 -0600

    slice

--- common/twilio.js
+++
@@ -1,7 +0,0 @@
-const accountSid = "ACbf760a670c4bf70307b8050664c1cfa8"
-const authToken = "494ce34901e6a26a371e984cb80c9643"
-const client = require("twilio")(accountSid, authToken)
-
-module.exports = {
-    client
-}
--- server/controller.js
+++ server/controller.js
@@ -6,7 +6,9 @@ admin.initializeApp({projectId: 'dailyreminders-62630', credential: admin.creden
 
 const firestore = admin.firestore();
 
-const { client } = require("../common/twilio")
+const accountSid = "ACbf760a670c4bf70307b8050664c1cfa8"
+const authToken = "494ce34901e6a26a371e984cb80c9643"
+const client = require("twilio")(accountSid, authToken)
 
 const docRef = firestore.collection('dailyTasks').doc('tasks')
 
