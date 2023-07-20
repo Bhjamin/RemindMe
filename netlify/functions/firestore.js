56089f48790c949c4b95cceb0026c63b1014cbd3
Ben Hancock <benjamin@hancock1.com>
2023-07-20 11:37:44 -0600

    esea

--- netlify/functions/firestore.js
+++ netlify/functions/firestore.js
@@ -1,9 +1,10 @@
 const { schedule } = require('@netlify/functions')
+const axios = require('axios')
 
 const handler = async (event, context) => {
 
     try {
-       const res = await fetch('https://remindme-beng.netlify.app/api/sendReminder')
+       const res = await axios.get('https://remindme-beng.netlify.app/api/sendReminder')
        console.log("RESPONSE: ", res)
     } catch (err) {
         console.log(err)
